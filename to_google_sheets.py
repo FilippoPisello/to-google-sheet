b61b9ef54b66046344ccfc420272c60261afac6f
Filippo Pisello <filippo.pisello@live.it>
2021-01-07 14:46:07 +0100

    Now "NaT" converted to str is replaced by ""

--- to_google_sheets.py
+++ to_google_sheets.py
@@ -123,7 +123,7 @@ class GoogleSheet(Spreadsheet):
         # Convert datetime columns into string
         for column in self.df.columns:
             if self.df[column].dtype in ["datetime64[ns]", "datetime64"]:
-                self.df[column] = self.df[column].astype(str)
+                self.df[column] = self.df[column].astype(str).str.replace("NaT", "")
         # Replace missing values with something else
         self.df.fillna(fill_na_with, inplace=True)
 
